198e866babb5c8131a469e82e7ff10c68c1cd4fd
saikeerthi555 <43855677+saikeerthi555@users.noreply.github.com>
2021-02-12 11:54:46 +0530

    Add button is pressed When the add button is pressed, the current data in the input should be The <ul> should display all the array items as <li>s
    added to an array.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,19 @@ import React, { useState } from "react";
 
 function App() {
   const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
 
   function handleChange(event) {
     const newValue = event.target.value;
     setInputText(newValue);
+  }
+
+  function addItem() {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
+    setInputText("");
+    //this is when u add an item the prevItem in the input box to get cleared
   }
 
   return (
@@ -15,13 +24,15 @@ function App() {
       </div>
       <div className="form">
         <input onChange={handleChange} type="text" value={inputText} />
-        <button>
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {items.map((todoItem) => (
+            <li>{todoItem}</li>
+          ))}
         </ul>
       </div>
     </div>
